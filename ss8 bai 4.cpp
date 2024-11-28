a29341b99425954a09cb95177f377b8646586e36
ChloeAmelinda <trangthichngunuong@gmail.com>
2024-11-28 23:52:55 +0700

    ss8 bai 4

---
+++ ss8 bai 4.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	int arr[3][4]={
+	{1,2,3,5},
+	{4,6,8,10},
+	{12,7,9,18},
+	};
+	int max=arr[0][0];
+	
+	for(int i = 0; i<3 ;i++)
+	{
+		for(int j = 0; i<4 ;j++)
+		{
+			if(max < arr[i][j])
+			{
+				max=arr[i][j];
+			}
+		}
+	}
+	printf("phan tu lon nhat la : %d\n", max);
+	
+	
+	return 0;
+}
+
+  
