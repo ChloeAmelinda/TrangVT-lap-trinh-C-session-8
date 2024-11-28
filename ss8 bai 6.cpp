146bd85264b21cb099ed8d3377665b56f471e838
ChloeAmelinda <trangthichngunuong@gmail.com>
2024-11-29 00:17:42 +0700

    ss8 bai 6

---
+++ ss8 bai 6.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int arr[5][5]={
+	 {12,23,34,45,56},
+	 {1,2,3,4,5},
+	 {2,4,6,8,10},
+	 {1,3,5,7,9},
+	 {10,20,30,40,50}
+	};
+	
+	int sum=0;
+	printf("duong cheo chinh la : ");
+	for(int i =0 ;i<5; i++)
+	{
+		printf(" %d ",arr[i][i]);
+		sum += arr[i][i];
+	}
+	printf("\n tong cua duong cheo chinh la : %d", sum);
+	return 0;
+}
